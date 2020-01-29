ad1a949f59fc0fe5f400d29b30e46cc89bf72133
Shelby <shelbymollineaux@gmail.com>
2020-01-28 20:07:35 -0500

    updating for state and UI

--- app.js
+++ app.js
@@ -1,50 +1,46 @@
 function onReady() {
+  const toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const newToDoText = document.getElementById('newToDoText');
-  const toDoList = document.getElementById('toDoList');
-  const clearButton = document.getElementById('clear');
-  // console.log(addToDoForm, newToDoText, toDoList)
-  addToDoForm.addEventListener('submit', (event) => {
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) { return; }
+
+    toDos.push({
+      title: newToDoText.value,
+      complete: false
+    });
+
+    newToDoText.value = '' ;
+
+    renderTheUI();
+  }
+
+  function renderTheUI() {
+    const toDoList = document.getElementById('toDoList');
+
+    toDoList.textContent = '';
+
+    toDos.forEach(function(toDo) {
+      const newLi = document.createElement('li');
+      const checkbox = document.createElement('input');
+      checkbox.type = "checkbox";
+
+      newLi.textContent = toDo.title;
+
+      toDoList.appendChild(newLi);
+      newLi.appendChild(checkbox);
+    });
+  }
+
+  addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-    let title = newToDoText.value;
-    console.log(title);
-    let newLi = document.createElement('li');
-    // create a new input
-    let checkbox = document.createElement('input');
-
-    // set the input's type to checkbox
-    checkbox.type = "checkbox"
-
-    newLi.textContent = title;
-    newLi.appendChild(checkbox);
-
-    let button = document.createElement('button');
-    button.textContent = 'delete';
-    newLi.prepend(button);
-
-    button.addEventListener('click', (event) => {
-      button.parentNode.remove();
-    })
-
-    toDoList.appendChild(newLi);
-    newToDoText.value = '';
+    createNewToDo();
   });
 
-  clearButton.addEventListener('click', (event) => {
-    // console.log(event)
-    let everythingChecked = document.querySelectorAll('input[type=checkbox]:checked')
-    // console.log(everythingChecked)
-    for (var i = 0; i<everythingChecked.length; i++){
-      let textBox = everythingChecked[i]
-      textBox.parentNode.remove();
-    }
-  })
-
-  console.log(clearButton)
-
+  renderTheUI();
 
 }
-
 
 window.onload = function() {
    // alert("The window has loaded!");
